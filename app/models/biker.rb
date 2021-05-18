c85f04e2352a204c16a2709253a7628086447232
Nathalie Loussouarn <natel@hotmail.com>
2021-05-18 15:38:56 +0200

    feat(cloudinary seed 1#)

--- app/models/biker.rb
+++ app/models/biker.rb
@@ -2,6 +2,7 @@ class Biker < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  has_one_attached :photo
 
   validates :pseudo, presence: true
   validates :address, presence: true
